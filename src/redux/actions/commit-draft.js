d75ed4add0d6a0859e1f59915dbb989389f44180
sixertoy <sixertoy.github@gmail.com>
2020-07-25 07:16:22 +0200

    update

--- src/redux/actions/commit-draft.js
+++ src/redux/actions/commit-draft.js
@@ -19,7 +19,8 @@ const commitDraft = points => {
   const id = uuidv1();
   const color = '#D94865';
   const name = ucFirst(getName());
-  const polygon = points.length > 2;
+  const polygon = false;
+  // const polygon = points.length > 2;
   const distance = getDistance(points);
   const data = { color, distance, id, name, points, polygon };
   return { data, type: EVENT_TYPES.DRAFT_COMMIT };
