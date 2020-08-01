448d2dd6916a644b68c884ad4ff4801acb43f8a8
sixertoy <sixertoy.github@gmail.com>
2020-08-01 06:15:38 +0200

    update reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,11 @@
 import { combineReducers } from 'redux';
 
 import * as app from './app';
+import * as parcours from './parcours';
 import user from './user';
 
 function createRootReducer() {
-  return combineReducers({ ...app, user });
+  return combineReducers({ ...app, ...parcours, user });
 }
 
 export default createRootReducer;
---
+++ src/redux/reducers/parcours.js
@@ -0,0 +1,46 @@
+import { EVENT_TYPES } from '../../constants';
+
+export const editmode = (state = false, action) => {
+  switch (action.type) {
+    case EVENT_TYPES.DRAFT_CREATE:
+      return true;
+    case EVENT_TYPES.DRAFT_CANCEL:
+    case EVENT_TYPES.DRAFT_COMMIT:
+      return false;
+    default:
+      return state;
+  }
+};
+
+export const draft = (state = {}, action) => {
+  switch (action.type) {
+    case EVENT_TYPES.DRAFT_CREATE:
+      return { ...action.data };
+    case EVENT_TYPES.DRAFT_CANCEL:
+    case EVENT_TYPES.DRAFT_COMMIT:
+      return {};
+    case EVENT_TYPES.DRAFT_ADD_POINT:
+      return { ...state, points: [...(state.points || []), action.latlng] };
+    case EVENT_TYPES.DRAFT_UPDATE:
+      return { ...state, ...action.data };
+    default:
+      return state;
+  }
+};
+
+export const parcours = (state = [], action) => {
+  switch (action.type) {
+    case EVENT_TYPES.DRAFT_COMMIT:
+      return [...state, action.data];
+    case EVENT_TYPES.PARCOURS_DELETE:
+      return state.filter(obj => obj.id !== action.id);
+    case EVENT_TYPES.PARCOURS_UPDATE:
+      return state.map(obj => {
+        const iscurrent = obj.id === action.data.id;
+        if (!iscurrent) return obj;
+        return { ...obj, ...action.data };
+      });
+    default:
+      return state;
+  }
+};
