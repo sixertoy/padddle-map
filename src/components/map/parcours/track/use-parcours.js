62546713ab3c1ee2aaafe9257ba80d70e1dc3113
sixertoy <sixertoy.github@gmail.com>
2020-08-17 17:55:22 +0200

    fix double click to open edit mode

--- src/components/map/parcours/track/use-parcours.js
+++ src/components/map/parcours/track/use-parcours.js
@@ -5,16 +5,15 @@ import { useDispatch, useSelector } from 'react-redux';
 import { enableEditMode, openSelected } from '../../../../redux/actions';
 
 const useSelectHandler = data => {
-  const { coordinates, id } = data;
+  const { coordinates, id, user } = data;
 
   const { map } = useLeaflet();
   const dispatch = useDispatch();
 
-  const [editable, setEditable] = useState(false);
   const [isSelected, setIsSelected] = useState(false);
   const [showDistances, setShowDistances] = useState(true);
 
-  const user = useSelector(_ => _.user);
+  const uid = useSelector(_ => _.user);
   const editmode = useSelector(_ => _.editmode);
   const selected = useSelector(_ => _.selected);
   const createmode = useSelector(_ => _.createmode);
@@ -26,10 +25,10 @@ const useSelectHandler = data => {
   }, [createmode, coordinates, id, dispatch, editmode, map]);
 
   const toggleEditHandler = useCallback(() => {
-    const isowner = user === id;
+    const isowner = user === uid;
     if (editmode || createmode || !isowner) return;
     dispatch(enableEditMode());
-  }, [createmode, id, dispatch, editmode, user]);
+  }, [createmode, dispatch, editmode, uid, user]);
 
   useEffect(() => {
     const next = selected && selected === id;
@@ -41,15 +40,7 @@ const useSelectHandler = data => {
     setShowDistances(next);
   }, [id, isSelected, selected]);
 
-  useEffect(() => {
-    if (!editable) {
-      const next = editmode && selected === data.id;
-      setEditable(next);
-    }
-  }, [data.id, editable, editmode, selected]);
-
   return {
-    editable,
     opacity: 1,
     selectHandler,
     showDistances,
