15bef6e1b51052ff1e14a81ccb4c0c4c6bf08ffd
sixertoy <sixertoy.github@gmail.com>
2020-08-20 20:19:08 +0200

    update login with facebook

--- src/components/modals/login-modal/facebook-login.jsx
+++ src/components/modals/login-modal/facebook-login.jsx
@@ -1,7 +1,25 @@
 import firebase from 'firebase/app';
 import get from 'lodash.get';
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props';
+import { FaFacebookSquare as BrandIcon } from 'react-icons/fa';
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+  button: {
+    background: '#4267B2',
+    borderRadius: 4,
+    color: '#FFF',
+    composes: ['flex-columns', 'flex-center', 'items-center', 'p12'],
+    maxWidth: '100%',
+    minWidth: '100%',
+    width: '100%',
+  },
+  icon: {
+    fontSize: '1.2rem',
+    marginLeft: 7,
+  },
+});
 
 function isUserEqual(userId, firebaseUser) {
   if (!firebaseUser) return false;
@@ -16,16 +34,20 @@ function isUserEqual(userId, firebaseUser) {
 }
 
 const FacebookLoginProviderComponent = function FacebookLoginProviderComponent() {
+  const classes = useStyles();
+
+  const [disabled, setDisabled] = useState(false);
+
   const checkLoginState = useCallback(event => {
     const userId = get(event, 'userID', null);
     if (userId) {
+      setDisabled(true);
       const unsubscribe = firebase.auth().onAuthStateChanged(user => {
         unsubscribe();
         if (!isUserEqual(userId, user)) {
           const accessToken = get(event, 'accessToken', null);
           const credChecker = firebase.auth.FacebookAuthProvider.credential;
           const credential = credChecker(accessToken);
-          // Sign in with the credential from the Facebook user.
           firebase
             .auth()
             .signInWithCredential(credential)
@@ -51,8 +73,13 @@ const FacebookLoginProviderComponent = function FacebookLoginProviderComponent()
       appId="288008652477160"
       callback={checkLoginState}
       render={({ onClick }) => (
-        <button type="button" onClick={onClick}>
-          This is my custom FB button
+        <button
+          className={classes.button}
+          disabled={disabled}
+          type="button"
+          onClick={onClick}>
+          <span>Connexion</span>
+          <BrandIcon className={classes.icon} />
         </button>
       )}
     />
