afe7e781258355753ab1f1098d9da0c4c62cd92f
sixertoy <sixertoy.github@gmail.com>
2020-08-20 23:46:31 +0200

    update redirecturi

--- src/components/modals/login-modal/facebook-login.jsx
+++ src/components/modals/login-modal/facebook-login.jsx
@@ -72,7 +72,7 @@ const FacebookLoginProviderComponent = function FacebookLoginProviderComponent()
       // autoLoad
       appId="288008652477160"
       callback={checkLoginState}
-      redirectUri="http://login.padddle.io"
+      redirectUri="https://fblogin.padddle.io"
       render={({ onClick }) => (
         <button
           className={classes.button}
