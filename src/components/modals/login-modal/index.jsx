07e139aed4d6e246f3b78a06c72f885be8d8949d
sixertoy <sixertoy.github@gmail.com>
2020-08-20 14:15:27 +0200

    revert

--- src/components/modals/login-modal/index.jsx
+++ src/components/modals/login-modal/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { useMediaQuery } from 'react-responsive';
 
+// import { useMediaQuery } from 'react-responsive';
 import { IS_DEVELOPMENT } from '../../../constants';
 import FacebookProvider from './facebook';
 import FacebookLoginProvider from './facebook-login';
 import GoogleProvider from './google';
 
 const LoginModalComponent = function LoginModalComponent() {
-  const isMobile = useMediaQuery({ query: '(max-width: 680px)' });
+  const isMobile = false;
+  // const isMobile = useMediaQuery({ query: '(max-width: 680px)' });
   return (
     <React.Fragment>
       {IS_DEVELOPMENT && <GoogleProvider />}
