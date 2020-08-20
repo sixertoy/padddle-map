05021d5dec2aa43f86b1094762cf464767932268
sixertoy <sixertoy.github@gmail.com>
2020-08-20 19:47:31 +0200

    add login with facebook

--- src/components/modals/login-modal/facebook-login.jsx
+++ src/components/modals/login-modal/facebook-login.jsx
@@ -1,10 +1,48 @@
-// import firebase from 'firebase/app';
+import firebase from 'firebase/app';
+import get from 'lodash.get';
 import React, { useCallback } from 'react';
 import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props';
 
+function isUserEqual(userId, firebaseUser) {
+  if (!firebaseUser) return false;
+  const providerData = get(firebaseUser, 'providerData', null);
+  const found = providerData.find(obj => {
+    const uid = get(obj, 'uid', null);
+    const providerId = get(obj, 'providerId', null);
+    const useFacebook = providerId === 'facebook.com';
+    return useFacebook && uid === userId;
+  });
+  return !!found;
+}
+
 const FacebookLoginProviderComponent = function FacebookLoginProviderComponent() {
   const checkLoginState = useCallback(event => {
-    console.log('event', event);
+    const userId = get(event, 'userID', null);
+    if (userId) {
+      const unsubscribe = firebase.auth().onAuthStateChanged(user => {
+        unsubscribe();
+        if (!isUserEqual(userId, user)) {
+          const accessToken = get(event, 'accessToken', null);
+          const credChecker = firebase.auth.FacebookAuthProvider.credential;
+          const credential = credChecker(accessToken);
+          // Sign in with the credential from the Facebook user.
+          firebase
+            .auth()
+            .signInWithCredential(credential)
+            .catch(() => {
+              // Handle Errors here.
+              // const errorCode = error.code;
+              // const errorMessage = error.message;
+              // The email of the user's account used.
+              // const { email } = error;
+              // The firebase.auth.AuthCredential type that was used.
+              // const { credential } = error;
+            });
+        }
+      });
+    } else {
+      firebase.auth().signOut();
+    }
   }, []);
 
   return (
