5c2c72d017525fe34ea0376cabe1d15802514bab
sixertoy <sixertoy.github@gmail.com>
2020-08-20 19:11:15 +0200

    add login with facebook

--- src/components/modals/login-modal/facebook-login.jsx
+++ src/components/modals/login-modal/facebook-login.jsx
@@ -1,93 +1,58 @@
 import firebase from 'firebase/app';
+import get from 'lodash.get';
 import React, { useCallback } from 'react';
 import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props';
-import { FaFacebookSquare as BrandIcon } from 'react-icons/fa';
-import { createUseStyles } from 'react-jss';
 
-const useStyles = createUseStyles({
-  button: {
-    background: '#4267B2',
-    borderRadius: 4,
-    color: '#FFF',
-    composes: ['flex-columns', 'flex-center', 'items-center', 'p12'],
-    maxWidth: '100%',
-    minWidth: '100%',
-    width: '100%',
-  },
-  icon: {
-    fontSize: '1.2rem',
-    marginLeft: 7,
-  },
-});
+function isUserEqual(userId, firebaseUser) {
+  if (!firebaseUser) return false;
+  const providerData = get(firebaseUser, 'providerData', null);
+  const found = providerData.find(obj => {
+    const uid = get(obj, 'uid', null);
+    const providerId = get(obj, 'providerId', null);
+    const useFacebook = providerId === 'facebook.com';
+    return useFacebook && uid === userId;
+  });
+  return !!found;
+}
 
 const FacebookLoginProviderComponent = function FacebookLoginProviderComponent() {
-  const classes = useStyles();
-
-  const isUserEqual = useCallback((facebookAuthResponse, firebaseUser) => {
-    if (firebaseUser) {
-      const { providerData } = firebaseUser;
-      // eslint-disable-next-line
-      for (let i = 0; i < providerData.length; i++) {
-        if (
-          providerData[i].providerId ===
-            firebase.auth.FacebookAuthProvider.PROVIDER_ID &&
-          providerData[i].uid === facebookAuthResponse.userID
-        ) {
-          // We don't need to re-auth the Firebase connection.
-          return true;
+  const checkLoginState = useCallback(event => {
+    const userId = get(event, 'userID', null);
+    if (userId) {
+      const unsubscribe = firebase.auth().onAuthStateChanged(user => {
+        unsubscribe();
+        if (!isUserEqual(userId, user)) {
+          const accessToken = get(event, 'accessToken', null);
+          const credChecker = firebase.auth.FacebookAuthProvider.credential;
+          const credential = credChecker(accessToken);
+          // Sign in with the credential from the Facebook user.
+          firebase
+            .auth()
+            .signInWithCredential(credential)
+            .catch(() => {
+              // Handle Errors here.
+              // const errorCode = error.code;
+              // const errorMessage = error.message;
+              // The email of the user's account used.
+              // const { email } = error;
+              // The firebase.auth.AuthCredential type that was used.
+              // const { credential } = error;
+            });
         }
-      }
+      });
+    } else {
+      firebase.auth().signOut();
     }
-    return false;
   }, []);
-
-  const checkLoginState = useCallback(
-    event => {
-      if (event.authResponse) {
-        // User is signed-in Facebook.
-        const unsubscribe = firebase.auth().onAuthStateChanged(firebaseUser => {
-          unsubscribe();
-          // Check if we are already signed-in Firebase with the correct user.
-          if (!isUserEqual(event.authResponse, firebaseUser)) {
-            // Build Firebase credential with the Facebook auth token.
-            const credential = firebase.auth.FacebookAuthProvider.credential(
-              event.authResponse.accessToken
-            );
-            // Sign in with the credential from the Facebook user.
-            firebase
-              .auth()
-              .signInWithCredential(credential)
-              .catch(() => {
-                // Handle Errors here.
-                // const errorCode = error.code;
-                // const errorMessage = error.message;
-                // The email of the user's account used.
-                // const { email } = error;
-                // The firebase.auth.AuthCredential type that was used.
-                // const { credential } = error;
-              });
-          } else {
-            // User is already signed-in Firebase with the correct user.
-          }
-        });
-      } else {
-        // User is signed-out of Facebook.
-        firebase.auth().signOut();
-      }
-    },
-    [isUserEqual]
-  );
 
   return (
     <FacebookLogin
       autoLoad
       appId="288008652477160"
       callback={checkLoginState}
-      redirectUri="https://padddle.io"
       render={({ onClick }) => (
-        <button className={classes.button} type="button" onClick={onClick}>
-          <span>Connexion</span>
-          <BrandIcon className={classes.icon} />
+        <button type="button" onClick={onClick}>
+          This is my custom FB button
         </button>
       )}
     />
--- src/components/modals/login-modal/index.jsx
+++ src/components/modals/login-modal/index.jsx
@@ -1,23 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-// import { useMediaQuery } from 'react-responsive';
 import { IS_DEVELOPMENT } from '../../../constants';
 import FacebookProvider from './facebook';
 import FacebookLoginProvider from './facebook-login';
 import GoogleProvider from './google';
 
 const LoginModalComponent = function LoginModalComponent() {
-  const isMobile = false;
-  // const isMobile = useMediaQuery({ query: '(max-width: 680px)' });
+  const debugMode = useSelector(_ => _.debugmode);
   return (
     <React.Fragment>
-      {IS_DEVELOPMENT && <GoogleProvider />}
-      {!IS_DEVELOPMENT && (
-        <React.Fragment>
-          {!isMobile && <FacebookProvider />}
-          {isMobile && <FacebookLoginProvider />}
-        </React.Fragment>
-      )}
+      {debugMode && <FacebookLoginProvider />}
+      {!debugMode && IS_DEVELOPMENT && <GoogleProvider />}
+      {!debugMode && !IS_DEVELOPMENT && <FacebookProvider />}
     </React.Fragment>
   );
 };
