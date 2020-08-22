66f365e5a4dd98d6088fd06abc979e8dcf47eaba
sixertoy <sixertoy.github@gmail.com>
2020-08-23 00:34:44 +0200

    restore mobile inapp login button

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -5,7 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import { version } from '../../../package.json';
 import { ReactComponent as SVG } from '../../assets/logo.svg';
-import { isFacebookApp } from '../../core/facebook';
+// import { isFacebookApp } from '../../core/facebook';
 import { IfFirebaseAuthed, IfFirebaseUnAuthed } from '../../core/firebase';
 import { disableDebugMode, enableDebugMode } from '../../redux/actions';
 import LoggedButton from './logged-button';
@@ -79,7 +79,8 @@ const HeaderComponent = React.memo(function HeaderComponent() {
     dispatch(disableDebugMode());
   }, [dispatch]);
 
-  const showLogin = debugmode || !isFacebookApp();
+  const showLogin = true;
+  // const showLogin = debugmode || !isFacebookApp();
 
   return (
     <div className={classes.container}>
