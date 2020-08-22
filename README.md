0c29e61291a7dd908401e1f7da431070be8b1e33
sixertoy <sixertoy.github@gmail.com>
2020-08-22 22:22:23 +0200

    add FB.getLoginState

--- README.md
+++ README.md
@@ -14,6 +14,9 @@ https://freedraw.herokuapp.com
 **Facebook App ID**
 288008652477160
 
+**Feacebook Redirect URI**
+https://padddle.io/?code=AQADFWyV9N9mYYa6lx9biEgsppn-6UGuTs7lZ-rDZd9NaBIyRAH_yAUWoJ3bXdRXp1n7zQQbeAf6UlM7EzxcIGQVHCuCQATr40Tkkgyr4_5Nu6gyQSfXtAqRK7AwSfk6SXVuaBw_xPcXFpVL3mbhsEwxIDJ1NRhRB-4WM5gY5BuZLyjG6INhRqfTl1xsRDtN6D0jrUbp7zPLbtL8mlcDrsHxcNG1RokyDlxqH_nSqELCkcLn3OuStX8aVQJIJU9qDz9hNBeI5xEcHyAUaCjIxs969OHsg4TFNL423dYih4BxlE8ZZPp3hWNazRaqRZkQlJ_WCPGU0eTpu4mBgmNsPe5b&state=facebookdirect#_=_
+
 **MapConfig Regex**
 ([0-9]+\.?[0-9]_?),([0-9]+\.?[0-9]_),?([0-9]\*)?
 
--- src/pages/fblogin.jsx
+++ src/pages/fblogin.jsx
@@ -1,28 +1,34 @@
 import get from 'lodash.get';
-import queryString from 'query-string';
-import React, { useEffect, useState } from 'react';
+// import queryString from 'query-string';
+import React, { useCallback, useEffect, useState } from 'react';
 
-const forceFacebookRedirect = () => {
-  const { origin, search } = window.location;
-  const query = queryString.parse(search);
-  const state = get(query, 'state');
-  if (state === 'facebookdirect') {
-    window.location.href = `${origin}/#/`;
-    //     window.location.href = `${origin}/#/fblogin/${search}`;
-  }
-};
+// const forceFacebookRedirect = () => {
+//   const { origin, search } = window.location;
+//   const query = queryString.parse(search);
+//   const state = get(query, 'state');
+//   if (state === 'facebookdirect') {
+//     window.location.href = `${origin}/#/`;
+//   }
+// };
 
 const FacebookLoginPageComponent = function FacebookLoginPageComponent() {
   const search = get(window, 'location.search', null);
-  if (search) forceFacebookRedirect();
+  // if (search) forceFacebookRedirect();
+  const checkUserFacebookLogin = useCallback(() => {
+    window.FB.getLoginStatus(response => {
+      console.log('response', response);
+      // statusChangeCallback(response);
+    });
+  }, []);
 
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
     if (!mounted) {
+      checkUserFacebookLogin();
       setMounted(true);
     }
-  }, [mounted]);
+  }, [checkUserFacebookLogin, mounted]);
 
   return (
     <div id="application-page" style={{ color: '#FFFFFF' }}>
