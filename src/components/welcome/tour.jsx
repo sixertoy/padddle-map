099deaf1ad5b281c156dd8677b2a12fab827b2d9
sixertoy <sixertoy.github@gmail.com>
2020-08-25 15:26:20 +0200

    update tour, authed/unauthed

--- src/components/welcome/tour.jsx
+++ src/components/welcome/tour.jsx
@@ -1,8 +1,22 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import Joyride from 'react-joyride';
+import { useSelector } from 'react-redux';
 
 import { ZINDEX } from '../../constants';
+import { selectDemoMode } from '../../redux/selectors';
+
+const JOYRIDE_AUTHED_STEPS = [
+  {
+    content: 'Login button',
+    disableBeacon: true,
+    target: '#tour-step-login',
+  },
+  {
+    content: 'satellite-view',
+    target: '#tour-step-satellite',
+  },
+];
 
 const JOYRIDE_UNAUTHED_STEPS = [
   {
@@ -21,13 +35,24 @@ const TOUR_OPTIONS = {
 };
 
 const TourComponent = function TourComponent({ run }) {
+  const demomode = useSelector(selectDemoMode);
+  const steps = demomode ? JOYRIDE_UNAUTHED_STEPS : JOYRIDE_AUTHED_STEPS;
   return (
     <Joyride
       continuous
+      disableCloseOnEsc
+      disableOverlayClose
       showProgress
       showSkipButton
+      locale={{
+        back: 'Préc.',
+        close: 'Fermer',
+        last: 'Last',
+        next: 'Suiv.',
+        skip: 'Passer',
+      }}
       run={run}
-      steps={JOYRIDE_UNAUTHED_STEPS}
+      steps={steps}
       styles={{ options: TOUR_OPTIONS }}
     />
   );
